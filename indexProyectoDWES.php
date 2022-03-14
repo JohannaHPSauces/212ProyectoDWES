da8ad9b52157c59d0ad35a18c50cbc7693b2d7f5
johanna <johanna.herpoz@educa.jcyl.es>
2022-03-14 10:09:55 +0100

    14/03/2022

---
+++ indexProyectoDWES.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+       /* 
+    Author     : johanna Herrero Pozuelo
+	
+*/
+
+
+body{
+	font-family:OCR A Std, monospace;
+	/*background-image: url(images/fondo222.jpg); */
+     /* background-image: url("https://acegif.com/wp-content/gif/snwflks-5.gif"); */
+	background-repeat:no-repeat;
+	background-color:black;
+        background-size: cover;
+	
+}
+
+h2{ 
+	font-family: '04b', sans-serif;
+	text-shadow: black 0.1em 0.1em 0.2em;
+	text-align:center;
+	padding:6px;
+	color:red;
+	background-color:blueviolet;
+	margin-top:0%;
+	margin-right:10%;
+	margin-left:10%;
+	border: 4px dotted palegreen;
+	border-radius: 35px 0px 35px 0px;
+	-moz-border-radius: 35px 0px 35px 0px;
+	-webkit-border-radius: 35px 0px 35px 0px;
+}
+
+/*caja debajo de titulo*/
+.caja1{
+	text-shadow: black 0.1em 0.1em 0.2em;
+	background-color:red;
+	margin-top:0%;
+	margin-right:10%;
+	margin-left:10%;
+        border: 4px inset yellow;
+	border-radius: 35px 0px 35px 0px;
+	-moz-border-radius: 35px 0px 35px 0px;
+	-webkit-border-radius: 35px 0px 35px 0px;
+}
+
+
+.contenedor{
+        height: 350px;width: 1250px;
+        /* border: 5px solid yellow; */
+        border-radius: 0%;
+        /*background:rgb(255, 0, 200);*/
+        margin: 0 auto;
+        margin-top: 20vh;
+        display: flex;
+
+}
+ul{
+        display: flex;
+        flex-direction: column;
+        background: black;
+        opacity: 0.8;
+        border-radius: 5%;
+        border:4px solid rgb(255, 0, 128);
+        width: 200%;height: 110%;
+        margin-top:-65px;
+        /* margin-top: 55px; */
+        /* margin-left: 100px; */
+}
+li a{
+    
+        font-weight: bold;
+        text-decoration: none;
+}
+li:first-letter {
+	font-size: 1.5em;
+}
+
+ul li {
+        
+        margin-top:5px ;
+        /* background-color: azure; */
+        list-style: none;
+        /* text-align: center; */
+        font-size: 20px;
+}
+ul li:hover{
+        width: 1180px;height: 35px;
+        background: lime;
+        color: purple;
+        text-decoration:none; 
+
+}
+ul li:visited{
+        color:red;
+}
+footer{
+       
+       background: blueviolet;
+       border-radius: 5px 5px 5px 5px;
+       font-weight: bold;
+       position: fixed;
+       bottom: -1px;
+       width: 100%;
+       height: 60px;
+       color: black;
+       text-align: center;
+       padding: 2px;
+       vertical-align: middle;
+}
+a img{
+        display: flex;
+	margin:auto;
+	width:35px;
+	height:35px;
+}
+strong{
+        font-size: 20px;
+}
+strong a{
+    color:black;
+    text-decoration: none;
+}
+strong a:hover{
+    color:blue;
+}
+
+</style>
+    </head>
+    <body>
+        <header>
+            <h2>DESARROLLO WEB ENTORNO SERVIDOR </h2> 
+			<div class="caja1"></div>
+        </header> 
+		<div class="contenedor">
+			<ul>
+                <li><a href="webroot/Ejercicios_T1.pdf" target="_blank" class="c1">TEMA 1: DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+                <li><a href="indext2.php"  class="c2">TEMA 2: INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y EXPLOTACIÓN</a></li>
+                <li><a href="../proyectoTema3/indext3.php" class="c3">TEMA 3: CARACTERÍSTICAS DEL LENGUAJE PHP  </a></li>
+		<li><a href="../proyectoTema4/indext4.php" class="c4">TEMA 4: TÉCNICAS DE ACCESO A DATOS EN PHP </a></li>
+                <li><a href="../212DWESProyectoTema5/index.php" class="c4">TEMA 5: DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO </a></li>
+		<li><a href="pag3_xml.html" class="c4">TEMA 6: PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</a></li>
+		<li><a href="pag3_xml.html" class="c4">TEMA 7: PROGRAMACIÓN DE SERVICIOS WEB </a></li>
+		<!--<li><a href="pag3_xml.html" class="c4">TEMA 8: DESARROLLO DE APLICACIONES WEB HÍBRIDAS  </a></li>
+		<li><a href="pag3_xml.html" class="c4">TEMA 9: DESARROLLO DE APLICACIONES WEB HÍBRIDAS </a></li>-->
+            </ul> 
+        </div>
+    <footer>
+        2020-21 I.E.S. Los sauces. ©Todos los derechos reservados. <strong> <a href="http://daw212.sauces.local/">Johanna Herrero Pozuelo</a></strong>
+            <a href="https://github.com/JohannaHPSauces"><img src="webroot/images/git.png" alt="" class="git"></a>
+    </footer>
+</body>
+</html>
